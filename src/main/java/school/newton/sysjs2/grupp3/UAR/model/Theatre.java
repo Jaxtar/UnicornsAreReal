5745ee0d5d2334d1a3ad24f868149c76edf77ca4
MusaeM <Mustafa.Marouf@hotmail.com>
2021-03-29 15:54:13 +0200

    Add jakarta + Theatre-Entity

--- src/main/java/school/newton/sysjs2/grupp3/UAR/model/Theatre.java
+++ src/main/java/school/newton/sysjs2/grupp3/UAR/model/Theatre.java
@@ -1,4 +1,38 @@
 package school.newton.sysjs2.grupp3.UAR.model;
 
+import com.vaadin.flow.component.template.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+
+@Entity
 public class Theatre {
+
+    @Id
+    @GeneratedValue
+    public Integer theatreID;
+
+    public String theatreName;
+
+    public Theatre(Integer theatreID, String theatreName) {
+        this.theatreID = theatreID;
+        this.theatreName = theatreName;
+    }
+
+    public Integer getTheatreID() {
+        return theatreID;
+    }
+
+    public void setTheatreID(Integer theatreID) {
+        this.theatreID = theatreID;
+    }
+
+    public String getTheatreName() {
+        return theatreName;
+    }
+
+    public void setTheatreName(String theatreName) {
+        this.theatreName = theatreName;
+    }
+
+
 }
