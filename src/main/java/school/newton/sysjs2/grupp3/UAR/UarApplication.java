f6fdfdedc8e00f3e43045e07e9edb14d9cc17ce4
Jaxtar <39218584+Jaxtar@users.noreply.github.com>
2021-03-29 11:38:19 +0200

    gitignore test

--- src/main/java/school/newton/sysjs2/grupp3/UAR/UarApplication.java
+++ src/main/java/school/newton/sysjs2/grupp3/UAR/UarApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UarApplication {
 
+	//Start
 	public static void main(String[] args) {
 		SpringApplication.run(UarApplication.class, args);
 	}
