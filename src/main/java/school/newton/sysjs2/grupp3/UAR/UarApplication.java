2e0a76bf6ca0a9b3d43fe1b6acfdd8f3e1f86a28
Jaxtar <39218584+Jaxtar@users.noreply.github.com>
2021-03-29 10:31:36 +0200

    Add Spring Boot
    Added Spring Boot initializer

---
+++ src/main/java/school/newton/sysjs2/grupp3/UAR/UarApplication.java
@@ -0,0 +1,13 @@
+package school.newton.sysjs2.grupp3.UAR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UarApplication.class, args);
+	}
+
+}
