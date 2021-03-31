e90cc78e484764a999ed4d8133501f521d9c8aac
Jaxtar <39218584+Jaxtar@users.noreply.github.com>
2021-03-31 12:20:14 +0200

    Fix CSS
    Fixed CSS and broke out the navber form the rest

---
+++ src/main/java/school/newton/sysjs2/grupp3/UAR/UI/views/MainPage.java
@@ -0,0 +1,16 @@
+package school.newton.sysjs2.grupp3.UAR.UI.views;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import school.newton.sysjs2.grupp3.UAR.UI.Navbar;
+
+@Route(value="", layout=Navbar.class)
+@PageTitle("Unicorns Are Real!")
+public class MainPage extends VerticalLayout{
+    public MainPage(){
+        H1 test = new H1("Test");
+        add(test);
+    }
+}
