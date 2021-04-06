48748b7bb721806834840430a3c4b3e590fc29bc
VerGui <65910571+VerGui@users.noreply.github.com>
2021-04-06 23:33:52 +0200

    Add a Staff Login portal with views for staff info and schedule. Add a security package to make login work

---
+++ src/main/java/school/newton/sysjs2/grupp3/UAR/UI/views/StaffLoginView.java
@@ -0,0 +1,32 @@
+package school.newton.sysjs2.grupp3.UAR;
+
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.login.LoginForm;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.router.BeforeEnterObserver;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@Route("staff/login")
+@PageTitle("Login - Unicorns Are Real Staff")
+public class StaffLoginView extends VerticalLayout implements BeforeEnterObserver {
+    private LoginForm loginForm = new LoginForm();
+
+    public StaffLoginView() {
+        addClassName("login-view");
+        setSizeFull();
+        setAlignItems(Alignment.CENTER);
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        loginForm.setAction("login");
+        add(new H1("Unicorns Are Real"), loginForm);
+    }
+
+    @Override
+    public void beforeEnter(BeforeEnterEvent beforeEnterEvent){
+        if (beforeEnterEvent.getLocation().getQueryParameters().getParameters().containsKey("error")){
+            loginForm.setError(true);
+        }
+    }
+}
---
+++ src/main/java/school/newton/sysjs2/grupp3/UAR/UI/views/StaffView.java
@@ -0,0 +1,67 @@
+package school.newton.sysjs2.grupp3.UAR;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import org.apache.commons.lang3.StringUtils;
+import school.newton.sysjs2.grupp3.UAR.model.Staff;
+import school.newton.sysjs2.grupp3.UAR.repository.StaffRepository;
+
+import java.util.Collection;
+
+@Route(
+        value = "staff/info",
+        layout = school.newton.sysjs2.grupp3.UAR.StaffLayout.class)
+@PageTitle("Staff - Info - Unicorns Are Real")
+public class StaffView extends VerticalLayout {
+
+    private final StaffRepository repo;
+    //private final StaffEditor editor;
+    final Grid<Staff> grid;
+    final TextField filter;
+    private final Button addNewButton;
+
+    public StaffView(StaffRepository repo){//, StaffEditor editor) {
+        this.repo = repo;
+        //this.editor = editor;
+        this.grid = new Grid<>(Staff.class);
+        this.filter = new TextField();
+        this.addNewButton = new Button("New employee", VaadinIcon.PLUS.create());
+
+        HorizontalLayout actions = new HorizontalLayout(filter, addNewButton);
+        add(actions, grid);//, editor);
+
+        grid.setHeight("700px");
+        grid.setColumns("staffid", "firstname", "lastname", "email");
+        grid.getColumnByKey("staffid").setWidth("50px").setFlexGrow(0);
+        filter.setPlaceholder("Filter by last name");
+        filter.setValueChangeMode(ValueChangeMode.EAGER);
+        filter.addValueChangeListener(e -> listEmployee(e.getValue()));
+
+        grid.asSingleSelect().addValueChangeListener((e) -> {
+            //editor.editStaff(e.getValue());
+        });
+        this.addNewButton.addClickListener((e) -> {
+            //editor.editStaff(new Staff());
+        });
+        //editor.setChangeHandler(() -> {
+        //editor.setVisible(false);
+        //this.listEmployee(this.filter.getValue());
+        //});
+        this.listEmployee(null);
+    }
+    void listEmployee(String filterText) {
+        if (StringUtils.isEmpty(filterText)) {
+            this.grid.setItems((Collection<Staff>) repo.findAll());
+        }
+        else {
+            grid.setItems(repo.findByLastnameStartsWithIgnoreCase(filterText));
+        }
+    }
+}
