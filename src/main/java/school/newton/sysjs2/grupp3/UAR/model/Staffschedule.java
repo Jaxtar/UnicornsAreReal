825d347418a8340bf87af25a351797845c99131d
MusaeM <Mustafa.Marouf@hotmail.com>
2021-04-09 18:21:55 +0200

    Change ID-import

--- src/main/java/school/newton/sysjs2/grupp3/UAR/model/Staffschedule.java
+++ src/main/java/school/newton/sysjs2/grupp3/UAR/model/Staffschedule.java
@@ -1,10 +1,9 @@
 package school.newton.sysjs2.grupp3.UAR.model;
 
-import com.vaadin.flow.component.template.Id;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.sql.Time;
